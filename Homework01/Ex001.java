5b1fe1175369bb36dc273118574d2d61b30622bd
Sergei Kamianetskii <i@ksergey.ru>
2023-02-13 20:33:14 +0300

    Внесены правки в основной файл

--- Homework01/Ex001.java
+++ Homework01/Ex001.java
@@ -2,14 +2,12 @@ package Homework01;
 
 public class Ex001 {
   public static void main(String[] args) {
-    
 
   }
-  
-  void printmass(int[] arg) {
-    
-    for (int i = 0; i < arg.length; i++) {
-      System.out.println(arg[i]);
+
+  void print(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.println(array[i]);
     }
   }
 }
