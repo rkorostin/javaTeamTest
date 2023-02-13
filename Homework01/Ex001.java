d6cfa64b831a58596d0e272f52c93173cd8bd8d4
ilya <ilya_1_3_5@mail.ru>
2023-02-13 22:25:46 +0500

    print massiv added

--- Homework01/Ex001.java
+++ Homework01/Ex001.java
@@ -2,6 +2,14 @@ package Homework01;
 
 public class Ex001 {
   public static void main(String[] args) {
+    
 
+  }
+  
+  void printmass(int[] arg) {
+    
+    for (int i = 0; i < arg.length; i++) {
+      System.out.println(arg[i]);
+    }
   }
 }
